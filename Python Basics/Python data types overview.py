82c68f7c6a6a6b7c5fbfff91bd91b290363bd56c
ParkerKristy <122040121+ParkerKristy@users.noreply.github.com>
2023-01-28 15:55:45 +0100

    Day 2

---
+++ Python Basics/Python data types overview.py
@@ -0,0 +1,17 @@
+# Fundamental Data types
+
+int         #integer - numbers
+float       #decimal numbers
+bool        #bollean True x False
+str         #letters
+list
+tuple
+set
+dict
+
+#Classes -> custom types
+
+#Specialized Data Types -> not build into Python, special packages, modules used from the libraries
+#Modules
+
+None #nothing, absence of value
