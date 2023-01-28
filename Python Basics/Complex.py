6aff6f1141fd15422f5643e47365acaba245b6be
ParkerKristy <122040121+ParkerKristy@users.noreply.github.com>
2023-01-28 15:58:08 +0100

    Day 2

---
+++ Python Basics/Complex.py
@@ -0,0 +1,7 @@
+# Data type complex
+# most of the time you won't use it
+# just to know that it is 3rd type next to int and flow
+complex
+
+print(bin(5)) #binary interpetation of an integer
+print(int('0b101', 2))
---
+++ Python Basics/Numbers.py
@@ -0,0 +1,48 @@
+# Numbers -  int & float
+
+#integer 
+#float - decimal number
+
+print(type(2 + 4))
+print(2 + 4)
+print(type(2 + 4))
+print(2 - 4)
+print(type(2 - 4))
+print(2 * 4)
+print(type(2 * 4))
+print(2 / 4)
+print(type(2 / 4)) #float 0.5
+
+print(type(0.0001)) 
+print(type(0)) #int
+print(type(20 + 1.1))
+print(type(9.9 + 1.1)) #float 11.0
+
+print(2 ** 3)
+print(2 //4)
+
+# modulo
+print(5 % 4) # 1 remainger
+
+# Math functions https://www.programiz.com/python-programming/modules/math
+print(round(3.1)) #3
+print(abs(-20)) #absolute value of an argument
+
+# Operator precedence
+print(20 - 3 * 4) #8   *first  -second
+
+# ()
+# **
+# * /
+# + -
+
+#Operator precedence exercise
+print((100//7 ) ** 2 / 5 + 7 - 1.2)
+print(3 **2 * 5.0)
+print(6 ** 2 + 1.5 * 6)
+print(5 ** 2 * 1.5 - 5 * 2.5)
+print( 5**2)
+
+
+
+#docs.python.org - official Python documentation
